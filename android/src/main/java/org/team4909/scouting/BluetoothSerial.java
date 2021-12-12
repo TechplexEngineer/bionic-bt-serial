090b9be44258e5b8fcdb4f4b9b6324f9e0affa43
Blake Bourque <techplex.engineer@gmail.com>
2021-12-12 14:08:32 -0500

    Initial Scaffold

---
+++ android/src/main/java/org/team4909/scouting/BluetoothSerial.java
@@ -0,0 +1,11 @@
+package org.team4909.scouting;
+
+import android.util.Log;
+
+public class BluetoothSerial {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
